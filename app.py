4e77b68da7bcf3155ca812838f0ebfd74d4ba662
Syyad Khan <syykhan@deloitte.com>
2022-11-11 14:26:31 -0500

    converted to notebook

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
-def main():
-    print("hello pg")
+import pandas as pd
 
 
-if __name__ == "__main__":
-    main()
+fp = "/Users/syykhan/desktop/retail_db_json/order_items/part-r-00000-6b83977e-3f20-404b-9b5f-29376ab1419e"
+
+
+df = pd.read_json(fp, lines=True)
+
+print(df.count())
+print(df.describe())
+
+
+df.columns
+df.dtypes
+
+df[["order_item_order_id", "order_item_subtotal"]]
+
+df[df["order_item_order_id"] == 2]
